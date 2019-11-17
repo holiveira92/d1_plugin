b9f10f9f4721366f72e33f41d613a306e30e28cc
Hiago Oliveira <hiagojardimdeoliveira@hotmail.com>
2019-11-17 14:03:20 -0300

    criação de tabelas do BD automaticamente ao ativar o plugin

--- resources/scripts/bd.php
+++ resources/scripts/bd.php
@@ -11,10 +11,10 @@ function st_create_table() {
     // Se a tabela nao existe sera criada
     if ( $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) != $tablename ) {
         
-        $sql = "CREATE TABLE `$tablename` (
-        `id_` int(10) NOT NULL AUTO_INCREMENT,
-        `hit_ip` varchar_(100) NOT NULL,
-        `hit_date` datetime_ NOT NULL
+        $sql = "CREATE TABLE $tablename (
+        id_ int(10) NOT NULL AUTO_INCREMENT,
+        hit_ip varchar_(100) NOT NULL,
+        hit_date datetime_ NOT NULL
         );";
     
         // Ao usar a funcao dbDelta() e necessario carregar este ficheiro
@@ -30,135 +30,129 @@ add_action( 'init', 'st_create_table' );
 
 // trocar todos os collate para utf8_general_ci
 /*
-CREATE TABLE `wp_d1_cases` (
-	`id_card` INT(11) NOT NULL AUTO_INCREMENT,
-	`title_card` VARCHAR(250) NULL DEFAULT NULL,
-	`subtitle_card` VARCHAR(250) NULL DEFAULT NULL,
-	`text_footer_card` VARCHAR(250) NULL DEFAULT NULL,
-	`subtext_footer_card` VARCHAR(250) NULL DEFAULT NULL,
-	`card_link` VARCHAR(250) NULL DEFAULT NULL,
-	`img_bg_url` VARCHAR(250) NULL DEFAULT NULL,
-	`detach_card` INT(1) NOT NULL DEFAULT 0,
-	`desc_card` TEXT NULL DEFAULT NULL,
-	`impactos` TEXT NULL DEFAULT NULL,
-	`desc_completa_primaria` TEXT NULL DEFAULT NULL,
-	`desc_completa_secundaria` TEXT NULL DEFAULT NULL,
-	`objetivos_title` VARCHAR(250) NULL DEFAULT NULL,
-	`objetivos_desc_completa` TEXT NULL DEFAULT NULL,
-	`desafios` TEXT NULL DEFAULT NULL,
-	`implantacao` TEXT NULL DEFAULT NULL,
-	`cases_options` TEXT NULL DEFAULT NULL,
-	PRIMARY KEY (`id_card`)
+CREATE TABLE wp_d1_cases (
+	id_card INT(11) NOT NULL AUTO_INCREMENT,
+	title_card VARCHAR(250) NULL DEFAULT NULL,
+	subtitle_card VARCHAR(250) NULL DEFAULT NULL,
+	text_footer_card VARCHAR(250) NULL DEFAULT NULL,
+	subtext_footer_card VARCHAR(250) NULL DEFAULT NULL,
+	card_link VARCHAR(250) NULL DEFAULT NULL,
+	img_bg_url VARCHAR(250) NULL DEFAULT NULL,
+	detach_card INT(1) NOT NULL DEFAULT 0,
+	desc_card TEXT NULL DEFAULT NULL,
+	impactos TEXT NULL DEFAULT NULL,
+	desc_completa_primaria TEXT NULL DEFAULT NULL,
+	desc_completa_secundaria TEXT NULL DEFAULT NULL,
+	objetivos_title VARCHAR(250) NULL DEFAULT NULL,
+	objetivos_desc_completa TEXT NULL DEFAULT NULL,
+	desafios TEXT NULL DEFAULT NULL,
+	implantacao TEXT NULL DEFAULT NULL,
+	cases_options TEXT NULL DEFAULT NULL,
+	PRIMARY KEY (id_card)
 )
 COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
+ENGINE=InnoDB;
 
-CREATE TABLE `wp_d1_footer_links` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(300) NULL DEFAULT NULL,
-	`link` VARCHAR(300) NULL DEFAULT NULL,
-	`group_id` VARCHAR(50) NULL DEFAULT NULL,
-	`parent_id` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
-
-CREATE TABLE `wp_d1_faq` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`question` TEXT NULL DEFAULT NULL,
-    `answer` TEXT NULL DEFAULT NULL,
-    `page` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-;
-
-CREATE TABLE `wp_d1_call_to_action` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(250) NULL DEFAULT NULL,
-	`link` VARCHAR(250) NULL DEFAULT NULL,
-	`target` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
-
-CREATE TABLE `wp_d1_cases_categorias` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`descricao` VARCHAR(250) NULL DEFAULT NULL,
-	`id_categoria` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
-
-CREATE TABLE `wp_d1_key_points` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(250) NULL DEFAULT NULL,
-	`description` TEXT NULL DEFAULT NULL,
-	`url_img` TEXT NULL DEFAULT NULL,
-	`page` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-;
-
-CREATE TABLE `wp_d1_segmentos` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`main_title` VARCHAR(250) NULL DEFAULT NULL,
-	`title` VARCHAR(250) NULL DEFAULT NULL,
-	`description` TEXT NULL,
-	`url_img_bg` TINYTEXT NULL,
-	`challenge_title` VARCHAR(250) NULL DEFAULT NULL,
-	`challenge1` TEXT NULL,
-	`challenge2` TEXT NULL,
-	`challenge3` TEXT NULL,
-	`img_customer1` TEXT NULL,
-	`img_customer2` TEXT NULL,
-	`img_customer3` TEXT NULL,
-	`customers_title` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-;
-
-CREATE TABLE `wp_d1_seguranca` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(250) NULL DEFAULT NULL,
-	`description` TEXT NULL,
-	`description_alternative` TEXT NULL,
-	`url_img` TEXT NULL,
-	`tipo` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE wp_d1_footer_links (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(300) NULL DEFAULT NULL,
+	link VARCHAR(300) NULL DEFAULT NULL,
+	group_id VARCHAR(50) NULL DEFAULT NULL,
+	parent_id VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
 
-CREATE TABLE `wp_d1_modulos` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(250) NULL DEFAULT NULL,
-	`id_modulo` INT(11) NULL DEFAULT NULL,
-	`subtitle` VARCHAR(250) NULL DEFAULT NULL,
-	`description` TEXT NULL DEFAULT NULL,
-	`text_link` VARCHAR(250) NULL DEFAULT NULL,
-	`url_link` VARCHAR(250) NULL DEFAULT NULL,
-	`url_img` VARCHAR(250) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE wp_d1_faq (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	question TEXT NULL DEFAULT NULL,
+    answer TEXT NULL DEFAULT NULL,
+    page VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+;
+
+CREATE TABLE wp_d1_call_to_action (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	title VARCHAR(250) NULL DEFAULT NULL,
+	link VARCHAR(250) NULL DEFAULT NULL,
+	target VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
 
+CREATE TABLE wp_d1_cases_categorias (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	descricao VARCHAR(250) NULL DEFAULT NULL,
+	id_categoria VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE wp_d1_key_points (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	title VARCHAR(250) NULL DEFAULT NULL,
+	description TEXT NULL DEFAULT NULL,
+	url_img TEXT NULL DEFAULT NULL,
+	page VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+;
+
+CREATE TABLE wp_d1_segmentos (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	main_title VARCHAR(250) NULL DEFAULT NULL,
+	title VARCHAR(250) NULL DEFAULT NULL,
+	description TEXT NULL,
+	url_img_bg TINYTEXT NULL,
+	challenge_title VARCHAR(250) NULL DEFAULT NULL,
+	challenge1 TEXT NULL,
+	challenge2 TEXT NULL,
+	challenge3 TEXT NULL,
+	img_customer1 TEXT NULL,
+	img_customer2 TEXT NULL,
+	img_customer3 TEXT NULL,
+	customers_title VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+;
+
+CREATE TABLE wp_d1_seguranca (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	title VARCHAR(250) NULL DEFAULT NULL,
+	description TEXT NULL,
+	description_alternative TEXT NULL,
+	url_img TEXT NULL,
+	tipo VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE wp_d1_modulos (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	title VARCHAR(250) NULL DEFAULT NULL,
+	id_modulo INT(11) NULL DEFAULT NULL,
+	subtitle VARCHAR(250) NULL DEFAULT NULL,
+	description TEXT NULL DEFAULT NULL,
+	text_link VARCHAR(250) NULL DEFAULT NULL,
+	url_link VARCHAR(250) NULL DEFAULT NULL,
+	url_img VARCHAR(250) NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
 
 */
 ?>
